0adad9770abd84a2c8d8fbea4eb35ee961d96aa8
Z Michael Gehlke <zmgehlke@zmgsabstract.com>
2022-01-17 13:19:12 -0800

    Add an extra cube.

--- demo/render_5_cubes.py
+++ demo/render_5_cubes.py
@@ -4,7 +4,7 @@ from mathengine.tools.render.basic_grid import render
 
 
 def main():
-    for i in range(5):
+    for i in range(6): # This renders 0 (the point) to 5 (the 5-cube).
         model = Cube(i).model
         render(model, f"{i}-cube", "/data")    
     return
